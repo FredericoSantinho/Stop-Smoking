b0dd989853246bafdc773cbff829d4dd14b891b4
Neurophobic Animal <FredericoSantinho@gmail.com>
2023-04-12 16:47:17 +0100

    [SS-4] - Add GetStartOfDayRepositoryImplTest.kt.

---
+++ data/src/test/java/neuro/stop/smoking/data/repository/GetStartOfDayRepositoryImplTest.kt
@@ -0,0 +1,40 @@
+package neuro.stop.smoking.data.repository
+
+import kotlinx.coroutines.test.runTest
+import neuro.stop.smoking.data.dao.StartOfDayDao
+import neuro.stop.smoking.data.test.mocks.roomStartOfDayMock
+import neuro.stop.smoking.domain.test.mocks.startOfDayDtoMock
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+
+class GetStartOfDayRepositoryImplTest {
+
+	@Test
+	fun test() = runTest {
+		val startOfDayDao = mock<StartOfDayDao>()
+
+		val getStartOfDayRepository = GetStartOfDayRepositoryImpl(startOfDayDao)
+
+		whenever(startOfDayDao.getStartOfDay()).thenReturn(roomStartOfDayMock())
+
+		val startOfDayDto = getStartOfDayRepository.getStartOfDay()
+
+		assertEquals(startOfDayDtoMock(), startOfDayDto)
+	}
+
+	@Test
+	fun testNullStartOfDay() = runTest {
+		val startOfDayDao = mock<StartOfDayDao>()
+
+		val getStartOfDayRepository = GetStartOfDayRepositoryImpl(startOfDayDao)
+
+		whenever(startOfDayDao.getStartOfDay()).thenReturn(null)
+
+		val startOfDayDto = getStartOfDayRepository.getStartOfDay()
+
+		assertNull(startOfDayDto)
+	}
+}
